6ada39cd225cc01f10f79825f3818841ebb45154
yusungsamuel <ssyyu@ucdavis.edu>
2019-03-21 16:51:31 -0700

    added constructor function to letter.js

---
+++ Letter.js
@@ -0,0 +1,17 @@
+function Letter (character) {
+    this.character = character
+    this.guessed = false;
+    this.check = function (){
+        if (guessed){
+            return this.character
+        }
+        else {
+            return "_"
+        }
+    }
+    this.compare = function (attempt){
+        if (attempt === this.character){
+            this.guessed = true
+        }
+    }
+}
