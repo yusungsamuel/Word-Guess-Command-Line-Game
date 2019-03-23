93a1c157d731d68c04de848fe076fad9416c1852
yusungsamuel <ssyyu@ucdavis.edu>
2019-03-23 14:55:27 -0700

    add inquirer prompt to have user guess a letter with validation to check if user enter a letter and one letter only.

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var word = require("./Word.js")
 
 var inquirer = require("inquirer")
 
-var wordBank = ["ARTICUNO", "ZAPDOS", "MOLTRES", "MEWTWO", "RAIKOU", "ENTEI", "SUICUNE", "LUGIA", "LATIAS", "LATIOS", "KYOGRE", "GROUDON", "RAYQUAZA", "UXIE","MESPRIT", "AZELF","REGISTEEL", "REGICE", "REGIROCK"]
+var wordBank = ["ARTICUNO", "ZAPDOS", "MOLTRES", "MEWTWO", "RAIKOU", "ENTEI", "SUICUNE", "LUGIA", "LATIAS", "LATIOS", "KYOGRE", "GROUDON", "RAYQUAZA", "UXIE","MESPRIT", "AZELF","REGISTEEL", "REGICE", "REGIROCK"];
+
+var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
 var targetWord;
 
@@ -13,9 +15,23 @@ function start () {
     targetWord.createWord(randomWord)
     targetWord.printWord()
     
-    // inquirer.prompt([
-    //     {}
-    // ])
+    inquirer.prompt([
+        {
+            message: "Guess a letter",
+            name: "guess",
+            validate: function(input){
+                if (input.length !== 1 || alphabet.indexOf(input) === -1){
+                    return false
+                }
+                else {
+                    return true
+                }
+            }
+        }
+    ]).then(function(response){
+        targetWord.checkLetter(response.guess)
+        targetWord.printWord()
+    })
 }
 
 start()
