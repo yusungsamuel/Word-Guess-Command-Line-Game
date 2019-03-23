c1b14e727966062fd6f9dc41c40d25a5c8647628
yusungsamuel <ssyyu@ucdavis.edu>
2019-03-23 14:44:42 -0700

    write function to generate random word from word bank and use that random word to create a new Word Obj

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var Word = require("./Word.js")
+var word = require("./Word.js")
 
 var inquirer = require("inquirer")
 
 var wordBank = ["ARTICUNO", "ZAPDOS", "MOLTRES", "MEWTWO", "RAIKOU", "ENTEI", "SUICUNE", "LUGIA", "LATIAS", "LATIOS", "KYOGRE", "GROUDON", "RAYQUAZA", "UXIE","MESPRIT", "AZELF","REGISTEEL", "REGICE", "REGIROCK"]
 
+var targetWord;
+
 function start () {
     var randomWord = wordBank[Math.floor(Math.random()* wordBank.length)]
 
+    targetWord = new word.Word()
+    targetWord.createWord(randomWord)
+    targetWord.printWord()
     
+    // inquirer.prompt([
+    //     {}
+    // ])
 }
+
+start()
